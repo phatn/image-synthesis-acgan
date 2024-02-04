a477c5e6fff5c96a58ad36b881e69a72d21416a9
Henry Ha <106703135+henryhacode@users.noreply.github.com>
2024-02-03 21:47:55 -0600

    Implement Generator

---
+++ Generator.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+import torch
+
+
+class Generator(nn.Module):
+    def __init__(self, n_classes, latent_dim, img_size, channels):
+        super(Generator, self).__init__()
+
+        self.label_emb = nn.Embedding(n_classes, latent_dim)
+        self.init_size = img_size
+        self.l1 = nn.Sequential(nn.Linear(latent_dim, 128 * self.init_size ** 2))
+
+        self.conv_blocks = nn.Sequential(
+            nn.BatchNorm2d(128),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, channels, 3, stride=1, padding=1),
+            nn.Tanh()
+        )
+
+    def forward(self, noise, labels):
+        gen_input = torch.mul(self.label_emb(labels), noise)
+        out = self.l1(gen_input)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks(out)
+        return img
